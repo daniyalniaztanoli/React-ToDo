5341c7f4d19f1a8376ad9f1c2d5e40fe9095e759
MUHAMMAD DANIYAL <127001145+daniyalniaztanoli@users.noreply.github.com>
2024-01-13 17:37:42 +0500

    Update Forms.jsx

--- src/Components/Forms.jsx
+++ src/Components/Forms.jsx
@@ -57,11 +57,11 @@ export const Forms = () => {
     <div className='md:flex md:justify-center mx-10 mt-5 md:mx-0 md:mt-10 space-y-2 md:space-y-0 md:space-x-2'>
         <div className='flex flex-col'>
             <label className='md:text-2xl text-white'>Title : </label>
-            <input type="text" value={newtitle}  onChange={(e)=>setnewtitle(e.target.value)} placeholder='Enter Your Title Task.....' className='md:py-3 md:px-10 p-1 rounded focus:outline-2 focus:outline-cyan-500' />
+            <input type="text" value={newtitle}  onChange={(e)=>setnewtitle(e.target.value)} placeholder='Enter Your Title Task.....' className='md:py-3 md:pe-10 md:ps-4 p-1 rounded focus:outline-2 focus:outline-cyan-500' />
         </div>
         <div className='flex flex-col'>
         <label className='md:text-2xl text-white'>Description : </label>
-            <input type="text" value={newdescription} onChange={(e)=>setnewdescription(e.target.value)} placeholder='Enter Your Description Task.....' className='md:py-3 md:px-10 p-1 rounded focus:outline-2 focus:outline-cyan-500'/>
+            <input type="text" value={newdescription} onChange={(e)=>setnewdescription(e.target.value)} placeholder='Enter Your Description Task.....' className='md:py-3 md:pe-10 md:ps-4 p-1 text-left rounded focus:outline-2  focus:outline-cyan-500'/>
         </div>
         <div className='flex md:mt-7 justify-center'>
             <button type='button' onClick={Store_Handle_Value} className='md:py-3 md:px-6 md:mt-7 px-10 py-1 bg-cyan-400 rounded-xl hover:bg-cyan-500 text-white'>Add</button>
@@ -73,29 +73,28 @@ export const Forms = () => {
     
     
         {
-           todo.map((item,index)=>{
-                return(
-                    <><div className='flex justify-between mx-auto px-3 mt-4 border-b w-[80%]'>
-                    <div className='w-[80%] mt-3 rounded text-white' key={index}>
-                        <ul>
-                            <li>
-                            <h1 className='text-cyan-400  md:text-3xl'>{item.title}</h1>
-                            </li>
-                            <li>
+    todo.map((item, index) => {
+        return (
+            <div key={index} className='flex justify-between mx-auto px-3 mt-4 border-b w-[80%]'>
+                <div className='w-[80%] mt-3 rounded text-white'>
+                    <ul>
+                        <li>
+                            <h1 className='text-cyan-400 md:text-3xl'>{item.title}</h1>
+                        </li>
+                        <li>
                             <p>{item.description}</p>
-                            </li>
-                        </ul>
-                       
-                        
-                    </div><div className='flex space-x-2 mt-5 cursor-pointer'>
-                            <i onClick={()=>delete_handle(index)} className="fa-solid fa-trash text-white text-xl"></i>
-                            <i onClick={()=>edit_handle(index)} className="fa-solid fa-edit text-green-500 text-xl"></i>
-                        </div>
-                        </div>
-                        </>
-                ) 
-            })
-        }
+                        </li>
+                    </ul>
+                </div>
+                <div className='flex space-x-2 mt-5 cursor-pointer'>
+                    <i onClick={() => delete_handle(index)} className="fa-solid fa-trash text-white text-xl"></i>
+                    <i onClick={() => edit_handle(index)} className="fa-solid fa-edit text-green-500 text-xl"></i>
+                </div>
+            </div>
+        );
+    })
+}
+
         
     </div>
   )
